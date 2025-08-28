e0477b8be2555f5185d4aebb72d68fc854f1afa5
auracule007 <pogooluwa12@gmail.com>
2025-08-28 12:59:36 +0100

    updated settings

--- core/settings.py
+++ core/settings.py
@@ -167,8 +167,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "auracule034@gmail.com" 
-EMAIL_HOST_PASSWORD = "jhtakswbdwhmxttt"
+EMAIL_HOST_USER = "liveagoodlife1022@gmail.com" 
+EMAIL_HOST_PASSWORD = "abafggecwoonwvld"
 
 
 
