e5233661dd50413f3f2e71fa651fc90b97f0b3d7
auracule007 <pogooluwa12@gmail.com>
2025-09-09 14:17:36 +0100

    debug

--- core/settings.py
+++ core/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1#mok2zio!c##1ldk=*yl!v28f-!0ho75w42jjxg0a=7y_j#2q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*", "https://gatavalencc.onrender.com"]
 
