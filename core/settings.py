d99a40984d1261f5bb3a14bf5c86f4d12d8fd83a
auracule007 <pogooluwa12@gmail.com>
2025-09-12 12:48:35 +0100

    updated db

--- core/settings.py
+++ core/settings.py
@@ -91,12 +91,28 @@ CHANNEL_LAYERS = {
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'neondb',
+        'USER': 'neondb_owner',
+        'PASSWORD': 'npg_fv5xLkGt6eWN',
+        'HOST': 'ep-dawn-dust-ad6f80ww-pooler.c-2.us-east-1.aws.neon.tech',
+        'PORT': '5432',
+        # 'OPTIONS': {
+        #     'sslmode': 'require',
+        #     'channel_binding': 'disable',  # use 'require' if psycopg2 build supports it
+        # }
     }
 }
+
 
 
 # Password validation
